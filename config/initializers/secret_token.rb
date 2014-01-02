b68fd030b029acde42da1e9f5bb30a920d860d6f
Joey wang <wxluckly@gmail.com>
2014-01-02 22:11:29 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wanmenhua::Application.config.secret_key_base = 'd3ef642011f14caf284380eb63a95948805fd57284c23e7f2fdc78e9db23f09c810d3aef2963c75fbe0b5a450f52bde91b3df7a02576b051f7acbd47f5b6ae61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wanmenhua::Application.config.session_store :cookie_store, key: '_wanmenhua_session'

71dea73f947ab2ee0480a18a83c5e42bd7eda2ea
Joey wang <wxluckly@gmail.com>
2014-01-05 23:53:09 +0800

    coding

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < Admin::ApplicationController
   def index
-    @orders = Order.all
+    @orders = current_user.orders.paginate(page: params[:page])
   end
 
   def new
@@ -8,7 +8,19 @@ class Admin::OrdersController < Admin::ApplicationController
   end
 
   def create
-    @order = current_user.orders.new(params[:order])
+    @order = current_user.orders.create(order_params)
+    @client = Client.find_or_initialize_by(client_params)
+    @client.orders << @order
+    @client.save
+    redirect_to admin_orders_path
   end
 
+  private
+  def order_params
+    params.require(:order).permit(:status, :remark)
+  end
+
+  def client_params
+    params.require(:order).require(:client).permit(:cellphone, :name, :age, :gender, :email, :address, :exam_info)
+  end
 end

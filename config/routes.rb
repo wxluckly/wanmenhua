e20350a249644551d8db81f1408196b32a12b636
Joey wang <wxluckly@gmail.com>
2014-02-14 23:50:36 +0800

    coding

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Wanmenhua::Application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
+  resources :home, only: :index
+  resources :welcome, only: :index
+
   namespace "ajax" do
     get "get_client_by_cellphone"
     post "set_order_squad"
